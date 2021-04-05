21b7fa7fbe22a54ecfea6092ab8ab03a9014321b
Ashay Aswale <asaswale@wpi.edu>
2021-04-05 13:17:41 -0400

    Separate firefly generator

--- ex1.py
+++ ex1.py
@@ -17,9 +17,8 @@ def neighborFlashed(x,y):
     south = counters[x,max(y-1,0)]  >= T
     north = counters[x,min(y+1,size-1)]  >= T
     return east or west or south or north
-    
-def generateBoard(size):
-    global counters
+
+def getFireflies():
     global first
     fireflies = np.zeros([size,size])
     # Code Snippet
@@ -30,24 +29,31 @@ def generateBoard(size):
 
     for row in range(size):
         for col in range(size):
+            if counters[row,col] >= T:
+                fireflies[row,col] = 1.0
+    return fireflies
+
+def updateBoard(size):
+    global counters
+
+    for row in range(size):
+        for col in range(size):
             val = counters[row,col]
             counters[row,col] = val+1
 
             if(neighborFlashed(row,col)):
                 counters[row,col] = val + k*val
-
+            
             if val >= T:
-                fireflies[row,col] = 1
                 counters[row,col] = 0.0
-    # print(counters)
-    return fireflies
+                
+    return getFireflies()
 
 
-
-im = plt.imshow(generateBoard(size), animated=True,cmap='gray')
+im = plt.imshow(updateBoard(size), animated=True,cmap='gray')
 
 def updatefig(*args):
-    im.set_array(generateBoard(size))
+    im.set_array(updateBoard(size))
     return im,
 
 ani = animation.FuncAnimation(fig, updatefig, interval=90, blit=True)
